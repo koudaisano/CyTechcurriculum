5be7a9518bd59af26ee77ecf6951f3f0e46072b7
koudaisano <koukuncreator@gmail>
2024-07-27 17:21:28 +0900

    2nd_comit

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -4,16 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
-class DatabaseSeeder extends Seeder
+class ProductsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(productsTableSeeder::class);
+        // 10件の製品データを生成して挿入
+        Product::factory()->count(10)->create();
     }
 }
