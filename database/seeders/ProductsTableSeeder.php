fc07ae8882ca2eee67fb0e58fbb25bed6e40b487
koudaisano <koukuncreator@gmail>
2024-07-29 03:38:08 +0900

    ProductsSeeder use statement rearrangement

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
+
 
 class ProductsTableSeeder extends Seeder
 {
